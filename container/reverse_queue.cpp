e7204449f4b38a9139e15446c5566a2d58673c21
Phanendra Koppisetti <phanendra_koppisetti@yahoo.com>
2019-02-28 08:56:59 +0530

    Add files via upload

---
+++ container/reverse_queue.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+
+using namespace std;
+/*
+void showq(queue <int> gq)
+{
+	queue <int> g = gq;
+	while (!g.empty())
+	{
+		cout << '\t' << g.front();
+		g.pop();
+	}
+	cout << '\n';
+}
+
+void reverse(queue<int>& gquiz)
+{
+	stack<int> temp;
+	while(!gquiz.empty())
+	{
+		temp.push(gquiz.front());
+		gquiz.pop();
+	}
+
+	while(!temp.empty())
+	{
+		gquiz.push(temp.top());
+		temp.pop();
+	}
+}
+
+int main()
+{
+	queue <int> gquiz;
+	gquiz.push(10);
+	gquiz.push(20);
+	gquiz.push(30);
+
+	cout << "The queue gquiz is : ";
+	showq(gquiz);
+	reverse(gquiz);
+	cout << "The reverse of queue gquiz is : ";
+	showq(gquiz);
+    return 0;
+}*/
