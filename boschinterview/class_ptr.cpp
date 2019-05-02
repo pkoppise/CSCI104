456cba03dd78d98b18aea68e3a493e3cfd2e9495
Phanendra Koppisetti <phanendra_koppisetti@yahoo.com>
2019-05-02 16:04:32 +0530

    Update class_ptr.cpp

--- boschinterview/class_ptr.cpp
+++ boschinterview/class_ptr.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class A
+{
+public:
+	int a;
+	void func() {cout << "function called" << endl;}
+	static void func2() {cout << "static function called" << endl;}
+	//static void func3() {cout << "static function called with this" << endl; this-> a = 5;}
+	//static void func4() const {cout << "static function called with const" << endl; }
+	const static void func5() {cout << "static function called with const return" << endl; }
+	const static int func6() {cout << "static function called with const int return" << endl; return 1;}
+
+};
+
+int main()
+{
+	A *ptr = NULL;
+	ptr->func();
+	//ptr->a = 5; //segmentation error
+	ptr->func2();
+	//ptr->func3();  //compile error 
+	//ptr->func4();  //compile error
+	ptr->func5();
+	const int k = ptr->func6();
+	cout << k << endl;
+
+	static const int h = 10;
+	//h = 20;  //compile error
+	return 0;
+}
 
